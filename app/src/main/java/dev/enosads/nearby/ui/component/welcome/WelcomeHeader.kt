aee191954d714033415c9fe68de8f99368823107
Enos Andrade <enosads@gmail.com>
2024-12-14 00:19:10 -0300

    feat: add welcome screen
    - Created WelcomeScreen composable with header, content, and start button.
    - Created WelcomeHeader composable with logo and welcome message.
    - Created WelcomeContent composable with how-it-works tips.
    - Created WelcomeHowItWorksTip composable to display each tip.

---
+++ app/src/main/java/dev/enosads/nearby/ui/component/welcome/WelcomeHeader.kt
@@ -0,0 +1,40 @@
+package dev.enosads.nearby.ui.component.welcome
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import dev.enosads.nearby.R
+import dev.enosads.nearby.ui.theme.Typography
+
+@Composable
+fun WelcomeHeader(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_logo_logo_icon),
+            contentDescription = "Nearby App Logo",
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Boas vindas ao Nearby", style = Typography.headlineMedium)
+        Text(
+            text = "Tenho cupons de vantagem para usar em seus estabelecimentos favoritos",
+            style = Typography.bodyLarge
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun WelcomeHeaderPreview() {
+    WelcomeHeader()
+}
